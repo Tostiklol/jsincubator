b631ec576e7d7b43d8455da9e54a2f640d347e07
Tostiklol <egorikdod@gmail.com>
2025-12-31 14:43:24 +0300

    dadad

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -6,4 +6,19 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(str) {
+  const words = str.split(' ');
+  const capitalizedWords = [];
+for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+if (word.length > 0) {
+      const firstLetter = word[0].toUpperCase();
+      const restOfWord = word.slice(1).toLowerCase();
+      capitalizedWords.push(firstLetter + restOfWord);
+    } else {
+      capitalizedWords.push('');
+    }
+  }
+
+return capitalizedWords.join(' ');
+}
